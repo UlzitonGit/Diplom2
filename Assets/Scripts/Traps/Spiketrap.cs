456eea808dd174a66858bca55f6ac2a8abd895ca
Uzel <156241117+UlzitonGit@users.noreply.github.com>
2025-06-14 00:34:35 +0300

    wow

--- Assets/Scripts/Traps/Spiketrap.cs
+++ Assets/Scripts/Traps/Spiketrap.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,84 +7,18 @@ using UnityEngine.UI;
 
 public class Spiketrap : MonoBehaviour
 {
-    [SerializeField] private PlayerMovementAdvanced playerMovement;//
-    [SerializeField] protected PlayerCam playerCam;//
-    public GameObject RestartMenu;
+    UiLogic uiLogic;
     public LayerMask Player;
-
-    public Image[] images; // ���� ������ �����������, ��� ����� ���� ������
-    public float fadeDuration = 2f; // ����� �� ������ ����������
-    private Coroutine fadeCoroutine;
-    private bool isFullyVisible = false;
-
-    void Start()
+    private void Start()
     {
-        SetAlpha(0f);
-    }
-
-    void Update()
-    {
-        if (!isFullyVisible && Input.GetMouseButtonDown(0) && fadeCoroutine != null)
-        {
-            StopCoroutine(fadeCoroutine);
-            SetAlpha(1f);
-            isFullyVisible = true;
-        }
-    }
-
-    IEnumerator FadeIn()
-    {
-        float elapsed = 0f;
-        while (elapsed < fadeDuration)
-        {
-            float alpha = elapsed / fadeDuration;
-            SetAlpha(alpha);
-            elapsed += Time.deltaTime;
-            yield return null;
-        }
-        Time.timeScale = 0;
-        SetAlpha(1f);
-        isFullyVisible = true;
-    }
-
-    void SetAlpha(float alpha)
-    {
-        foreach (Image img in images)
-        {
-            Color c = img.color;
-            c.a = alpha;
-            img.color = c;
-        }
-    }
-    public void Restart()
-    {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
-        Time.timeScale = 1;
-    }
-    public void NextLevel()
-    {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
-        Time.timeScale = 1;
-    }
-    public void Menu()
-    {
-        SceneManager.LoadScene(0);
-        Time.timeScale = 1;
+        uiLogic = FindObjectOfType<UiLogic>();
     }
 
     private void OnTriggerEnter(Collider other)
     {
         if (((1 << other.gameObject.layer) & Player.value) != 0)
         {
-            playerMovement.enabled = false;//
-            playerCam.enabled = false;//
-            playerMovement.GetComponent<Rigidbody>().isKinematic = true;
-            RestartMenu.SetActive(true);
-            Cursor.lockState = CursorLockMode.None;
-            Cursor.visible = true;
-            fadeCoroutine = StartCoroutine(FadeIn());
+            uiLogic.Death();
         }
-
-
     }
 }
